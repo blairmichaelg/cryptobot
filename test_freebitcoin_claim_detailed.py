c1c1c4f415fd289b78c1b145a96b59c384f25ffc
Blair Michalg <blairmichaelg@gmail.com>
2026-02-05 20:53:56 -0600

    Fix import in FreeBitcoin test script

--- test_freebitcoin_claim_detailed.py
+++ test_freebitcoin_claim_detailed.py
@@ -15,7 +15,7 @@ project_root = Path(__file__).parent
 sys.path.insert(0, str(project_root))
 
 from browser.instance import BrowserManager
-from config.settings import BotSettings
+from core.config import BotSettings
 from faucets.freebitcoin import FreeBitcoinBot
 
 # Setup logging
